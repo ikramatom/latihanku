a6f65aae4da4249a5a8afbc0a7b464a7d50dc8da
ikramatom <ikramunicorn6@gmail.com>
2021-04-17 21:43:28 +0800

    perubahan

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 #ini halaman index
 
-@app.route("/index")
+@app.route("/")
 def index(): 
   return render_template("index.html") 
 
-""" cara di backend ke dua""" 
-""" @app.route('/detail/<uid>')
-def detail(uid):
-    mahasiswa = None
-    for m in daftar_mahasiswa:
-        if m['id'] == int(uid):
-            mahasiswa = m
-    return render_template('detail.html', mahasiswa=mahasiswa) """
 if __name__=="__main__":
   app.run(debug=True)
